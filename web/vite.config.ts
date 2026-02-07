9bbc6ac6b480e4532cd50c6794bf4f4d95527cf1
Dohun <dhgram09@gmail.com>
2026-02-07 19:46:56 +0900

    build: output to docs for GitHub Pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "/forBelle/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true
+  }
 });
