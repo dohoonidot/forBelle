20f7248d4f5d077a32c5465345360ea6f332cc32
Dohun <dhgram09@gmail.com>
2026-02-07 19:43:53 +0900

    fix: image paths and add favicon

--- web/src/songs.ts
+++ web/src/songs.ts
@@ -13,13 +13,13 @@ export const songs: Song[] = [
         title: "나랑아니면(who do you love)",
         videoId: "0dRo5Kbgx6c",
         vttUrl: "https://dohoonidot.github.io/forBelle/lyrics.vtt",
-        coverUrl: "/images/whodoyoulove.jpg",
+        coverUrl: "./images/whodoyoulove.jpg",
     },
     {
         id: "littleworld",
         title: "그대작은나의 세상이 되어 (You become my little world)",
         videoId: "hmP2yQoFrLM",
-        vttUrl: "/littleworld.vtt",
-        coverUrl: "/images/littleworld.jpg",
+        vttUrl: "./littleworld.vtt",
+        coverUrl: "./images/littleworld.jpg",
     }
 ];
